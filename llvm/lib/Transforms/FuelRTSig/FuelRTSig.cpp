b8dce4f57b5972d2b16f306599dc2759d805ae5b
germ3n <gasfggit@proton.me>
2025-05-24 14:52:56 +0100

    skip all in sync

--- llvm/lib/Transforms/FuelRTSig/FuelRTSig.cpp
+++ llvm/lib/Transforms/FuelRTSig/FuelRTSig.cpp
@@ -2298,11 +2298,7 @@ PreservedAnalyses FuelRTSigPass::run(Module &M, ModuleAnalysisManager &AM)
 
         //errs() << llFileBaseName << "::" << rustDemangle(F.getName().str()) << "\n";
 
-        if (strstr(rustDemangle(F.getName().str()).c_str(), "sys::pal::") ||
-            strstr(rustDemangle(F.getName().str()).c_str(), "::unix") ||
-            strstr(rustDemangle(F.getName().str()).c_str(), "thread::") ||
-            strstr(rustDemangle(F.getName().str()).c_str(), "threading::") ||
-            strstr(rustDemangle(F.getName().str()).c_str(), "core::") ||
+        if (
             strstr(rustDemangle(F.getName().str()).c_str(), "sync::")
         )
         {
